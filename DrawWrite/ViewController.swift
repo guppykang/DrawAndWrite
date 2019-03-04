acde208a4a6a57dbcecb325865c4af43c84a4fbb
Joshua Kang <joshuakang@joshuas-mbp-4.lan>
2019-03-04 01:44:08 -0800

    added code for drawing feature

--- DrawWrite/ViewController.swift
+++ DrawWrite/ViewController.swift
@@ -17,6 +17,19 @@ class ViewController: UIViewController {
     var swiped = false
     
     
+    let tempImageView: UIImageView = {
+        let theImageView = UIImageView()
+        theImageView.translatesAutoresizingMaskIntoConstraints = false //You need to call this property so the image is added to your view
+        return theImageView
+    }()
+    
+    let mainImageView: UIImageView = {
+        let theImageView = UIImageView()
+        theImageView.translatesAutoresizingMaskIntoConstraints = false //You need to call this property so the image is added to your view
+        return theImageView
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
@@ -24,7 +37,88 @@ class ViewController: UIViewController {
         
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    func setupLayout() {
+        view.addSubview(tempImageView)
+        view.addSubview(mainImageView)
+        
+        tempImageView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        tempImageView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        tempImageView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        tempImageView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        
+        
+        mainImageView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        mainImageView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        mainImageView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        mainImageView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else {
+            return
+        }
+        swiped = false
+        lastPoint = touch.location(in: view)
+    }
+    
+    func drawLine(from fromPoint: CGPoint, to toPoint: CGPoint) {
+        // 1
+        UIGraphicsBeginImageContext(view.frame.size)
+        guard let context = UIGraphicsGetCurrentContext() else {
+            return
+        }
+        tempImageView.image?.draw(in: view.bounds)
+        
+        // 2
+        context.move(to: fromPoint)
+        context.addLine(to: toPoint)
+        
+        // 3
+        context.setLineCap(.round)
+        context.setBlendMode(.normal)
+        context.setLineWidth(brushWidth)
+        context.setStrokeColor(color.cgColor)
+        
+        // 4
+        context.strokePath()
+        
+        // 5
+        tempImageView.image = UIGraphicsGetImageFromCurrentImageContext()
+        tempImageView.alpha = opacity
+        UIGraphicsEndImageContext()
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else {
+            return
+        }
+        
+        // 6
+        swiped = true
+        let currentPoint = touch.location(in: view)
+        drawLine(from: lastPoint, to: currentPoint)
+        
+        // 7
+        lastPoint = currentPoint
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if !swiped {
+            // draw a single point
+            drawLine(from: lastPoint, to: lastPoint)
+        }
+        
+        // Merge tempImageView into mainImageView
+        UIGraphicsBeginImageContext(mainImageView.frame.size)
+        mainImageView.image?.draw(in: view.bounds, blendMode: .normal, alpha: 1.0)
+        tempImageView.image?.draw(in: view.bounds, blendMode: .normal, alpha: opacity)
+        mainImageView.image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        tempImageView.image = nil
+    }
 
 }
 
